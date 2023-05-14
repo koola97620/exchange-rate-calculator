22876030a5813c81d6c94829c18dfe5f95b94905
koola97620 <koola97620@nate.com>
2023-05-15 04:52:11 +0900

    feat: 미사용코드 제거

--- src/main/java/com/exchangeratecalculator/infra/ExchangeRatesListApiResponse.java
+++ src/main/java/com/exchangeratecalculator/infra/ExchangeRatesListApiResponse.java
@@ -1,12 +1,9 @@
 package com.exchangeratecalculator.infra;
 
-import java.time.LocalDate;
 import java.util.Map;
 
 public record ExchangeRatesListApiResponse(
-        String base,
-        LocalDate date,
-        Map<String,Double> rates
+        Map<String, Double> rates
 ) {
     public Double exchangeRatesOfCountry(String targetCountry) {
         return rates.get(targetCountry);
